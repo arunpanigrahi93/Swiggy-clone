5dc58e82fc921d7ba23dbb846706e4bfc2688410
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-06-24 10:11:54 +0530

    removed counterslice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
-import counterReducer from "./counterSlice";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
-    counter: counterReducer,
   },
 });
